786f990729e4501496e60e4771303c0082517bc9
R_Matrix <dsq1961248553@sjtu.edu.cn>
2025-08-27 17:01:49 +0800

    初始化提交

---
+++ README.md
@@ -0,0 +1,1 @@
+# Some_GaoLian_Exercise
---
+++ SomeGaoLianExercise.lean
@@ -0,0 +1,10 @@
+-- This module serves as the root of the `SomeGaoLianExercise` library.
+-- Import modules here that should be built as part of the library.
+import SomeGaoLianExercise.Basic
+import SomeGaoLianExercise.Common_Inequality
+
+import Mathlib.Tactic
+import Mathlib.Util.Delaborators
+
+
+set_option warningAsError false
